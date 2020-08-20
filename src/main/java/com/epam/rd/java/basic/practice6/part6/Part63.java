5f2463de3826030aa1e677f999625acafa1b702b
jMushmyH <jMushmyH@email.com>
2020-08-21 00:35:56 +0300

    100

--- src/main/java/com/epam/rd/java/basic/practice6/part6/Part63.java
+++ src/main/java/com/epam/rd/java/basic/practice6/part6/Part63.java
@@ -3,7 +3,6 @@ package com.epam.rd.java.basic.practice6.part6;
 import com.epam.rd.java.basic.practice6.Demo;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Part63 {
 
@@ -13,14 +12,11 @@ public class Part63 {
 
     public static void duplicatesTask(String filePath) {
         List<String> inputList = Arrays.asList(Demo.readFileIntoStringArray(filePath));
-        List<String> listOfDuplicates = inputList.stream()
-                                        .filter(x -> Collections.frequency(inputList, x) > 1)
-                                        .collect(Collectors.toList());
 
-        for (int i = 0; i < 3; i++) {
-            System.out.println(new StringBuilder(listOfDuplicates.get(i).toUpperCase()).reverse());
-        }
-
+        inputList.stream()
+                .filter(x -> Collections.frequency(inputList, x) > 1)
+                .limit(3)
+                .forEach(x -> System.out.println(new StringBuilder(x.toUpperCase()).reverse()));
 
     }
 }
